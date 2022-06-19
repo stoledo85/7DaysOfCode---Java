b6008e808a9cb51b75fd530a89b11f2087511fff
Sander Toledo <sanderaugusto@gmail.com>
2022-06-19 20:47:08 -0300

    Correção do Dia1

--- src/Dia1.java
+++ src/Dia1.java
@@ -1,24 +1,24 @@
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.net.http.HttpClient;
 
 /**
- * Dia 1: Usar essa API para pesquisar os top 250 filmes e imprimir o JSON correspondente.
+ * Dia 1: Usar essa API para pesquisar os top 250 filmes e imprimir o JSON
+ * correspondente.
  */
 public class Dia1 {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create("https://imdb-api.com/pt_br/API/Top250Movies/k_g23sugoz"))
-                .build();
+    public static void main(String[] args) throws Exception {
+        String apiKey = "k_g23sugoz";
+        URI apiIMDB = URI.create("https://imdb-api.com/en/API/Top250TVs/" + apiKey);
 
-        HttpClient HTTPclient = HttpClient.newBuilder()
-                .build();
-        HttpResponse<String> response = HTTPclient.send(request, BodyHandlers.ofString());
-        System.out.println(response.body());
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(apiIMDB).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+
+        System.out.println("Resposta: " + json);
     }
 }
