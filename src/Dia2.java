ae5b3cc5cf9b7acaa07ff36b10c424757bb20252
Sander Toledo <sanderaugusto@gmail.com>
2022-06-20 10:37:40 -0300

    Inicio do dia 2

---
+++ src/Dia2.java
@@ -0,0 +1,20 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Dia2 {
+    public static void main(String[] args) throws Exception {
+        String apiKey = "k_g23sugoz";
+        URI apiIMDB = URI.create("https://imdb-api.com/en/API/Top250TVs/" + apiKey);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(apiIMDB).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+
+        System.out.println("Resposta: " + json);
+    }
+
+}
